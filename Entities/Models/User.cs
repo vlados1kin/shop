f2b1030a9ac77aeb781ee7087fa97d88a369c1f8
Vladislav Lashkin <lashkin2004@gmail.com>
2024-09-18 00:16:58 +0300

    add new properties to user model

--- Entities/Models/User.cs
+++ Entities/Models/User.cs
@@ -5,4 +5,6 @@ namespace Entities.Models;
 public class User : IdentityUser<Guid>
 {
     public string Name { get; set; }
+    public string? RefreshToken { get; set; }
+    public DateTime RefreshTokenExpiryTime { get; set; }
 }
