30d3e3e77dc86c8d52bce00fef6c17182c7ebbee
Vladislav Lashkin <lashkin2004@gmail.com>
2024-09-18 00:08:46 +0300

    add not found exceptions

---
+++ Entities/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Entities.Exceptions;
+
+public sealed class UserNotFoundException : NotFoundException
+{
+    public UserNotFoundException(Guid id) : base($"The user with id: {id} does not exist in the database.")
+    {
+    }
+}
