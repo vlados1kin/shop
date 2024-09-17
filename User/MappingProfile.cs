5c5ae5b4498dd9d42fd83d45cb46fd0013833961
Vladislav Lashkin <lashkin2004@gmail.com>
2024-09-18 00:14:52 +0300

    add mapping profile

---
+++ User/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using User.DTO;
+
+namespace User;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Entities.Models.User, UserDto>();
+        CreateMap<UserForRegistrationDto, Entities.Models.User>();
+        CreateMap<UserForUpdateDto, Entities.Models.User>();
+    }
+}
