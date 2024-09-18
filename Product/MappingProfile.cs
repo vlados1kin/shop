180925e06478d2fcb4e5f948d4c2fc3ef7aac008
Vladislav Lashkin <lashkin2004@gmail.com>
2024-09-18 03:43:35 +0300

    create mapping profile for product

---
+++ Product/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Product.DTO;
+
+namespace Product;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Entities.Models.Product, ProductDto>();
+        CreateMap<ProductForCreationDto, Entities.Models.Product>();
+        CreateMap<ProductForUpdateDto, Entities.Models.Product>();
+    }
+}
