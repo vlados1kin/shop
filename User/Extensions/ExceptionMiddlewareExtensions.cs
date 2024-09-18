0c2e5ed5db7844b7c687995ac7c83746279d467b
Vladislav Lashkin <lashkin2004@gmail.com>
2024-09-18 03:46:12 +0300

    add new type of exceptions to middleware

--- User/Extensions/ExceptionMiddlewareExtensions.cs
+++ User/Extensions/ExceptionMiddlewareExtensions.cs
@@ -21,6 +21,7 @@ public static class ExceptionMiddlewareExtensions
                     context.Response.StatusCode = contextFeature.Error switch
                     {
                         NotFoundException => StatusCodes.Status404NotFound,
+                        BadRequestException => StatusCodes.Status400BadRequest,
                         _ => StatusCodes.Status500InternalServerError
                     };
                     await context.Response.WriteAsync(new ErrorDetails()
