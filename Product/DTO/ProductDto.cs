0034eec59fa3e43c4280cf79e8223b886727650e
Vladislav Lashkin <lashkin2004@gmail.com>
2024-09-18 03:42:26 +0300

    create dto for product

---
+++ Product/DTO/ProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Product.DTO;
+
+public record ProductDto
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public string Title { get; init; }
+    public double Price { get; init; }
+    public bool IsVisible { get; init; }
+    public DateTime CreationTime { get; init; }
+    public Guid UserId { get; init; }
+}
---
+++ Product/DTO/ProductForCreationDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Product.DTO;
+
+public record ProductForCreationDto : ProductForManipulationDto
+{
+    
+}
