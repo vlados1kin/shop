a46a3512c22a1a2629eb4e5bc2d0dc34ab427de1
Vladislav Lashkin <lashkin2004@gmail.com>
2024-09-18 03:42:55 +0300

    create product service

---
+++ Product/Contracts/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using Product.DTO;
+using Shared.Features;
+
+namespace Product.Contracts;
+
+public interface IProductService
+{
+    Task<(IEnumerable<ProductDto> users, MetaData metaData)> GetProductsAsync(Guid id, ProductParameters productParameters, bool trackChanges);
+    Task<ProductDto> GetProductAsync(Guid id, Guid productId, bool trackChanges);
+    Task<ProductDto> CreateProductAsync(Guid id, ProductForCreationDto productForCreationDto);
+    Task UpdateProductAsync(Guid id, Guid productId, ProductForUpdateDto productForUpdateDto, bool trackChanges);
+    Task DeleteProductAsync(Guid id, Guid productId, bool trackChanges);
+}
